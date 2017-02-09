dc010064f7d731abcd0807175ef50267d328285e
lilindegit <2534725767@qq.com>
2017-02-09 08:55:31 +0800

    jianjian

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,8 +3,28 @@ namespace app\index\controller;
 use think\Controller;
 class Index extends Controller
 {
+    /**
+     * 首页
+     * @return mixed
+     */
     public function index()
     {
         return $this->fetch('index');
+    }
+
+    /**
+     * 房间列表
+     * @return mixed
+     */
+    public function edit(){
+        return $this->fetch('edit');
+    }
+
+    /**
+     * 直播房间
+     * @return mixed
+     */
+    public function room(){
+        return $this->fetch('room');
     }
 }
